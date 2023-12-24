8ac666c54c7584e4d49c7983359c98436007d62f
antonio celano <antonio_celano@hotmail.com>
2023-12-24 20:06:25 +0100

    Add an option to delete all records and and option to restore default records

--- client/src/components/RecordList.jsx
+++ client/src/components/RecordList.jsx
@@ -10,14 +10,23 @@ export default function RecordList() {
   const [loading, setLoading] = useState(true);
   const isMobile = window.outerWidth < 768;
 
-  // This method fetches the records from the database:
-  useEffect(() => {
+  // Fetch records function:
+  const fetchRecords = () => {
     fetch("https://mern-list-of-employees-api.vercel.app/home")
       .then((res) => res.json())
       .then((data) => {
         setRecords(data);
         setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching records:", error);
+        setLoading(false);
       });
+  };
+
+  // Fetch records on component mount:
+  useEffect(() => {
+    fetchRecords();
   }, []);
 
   // This method will delete a record:
@@ -56,6 +65,111 @@ export default function RecordList() {
       });
   }
 
+  // This method will delete all records:
+  function deleteAllRecords() {
+    fetch("https://mern-list-of-employees-api.vercel.app/home", {
+      method: "DELETE"
+    })
+
+      .catch(error => {
+        MySwal.fire({
+          icon: "error",
+          title: error
+        });
+        return;
+      })
+
+      // Since the fetch() function returns a Response object,
+      // we can retrieve the response as JSON using the json() function of the Response object.
+      // The json() method returns a promise which resolves with the result of parsing the body text as JSON.
+      // Note that despite the method being named json(), the result is not JSON
+      // but is instead the result of taking JSON as input and parsing it to produce a JavaScript object.
+      // This allows us to handle the data being sent down to the client-side like a JavaScript object:
+      .then((res) => res.json())
+
+      // Once we have the data returned to us, we get the message property and then put it in a variable called data.
+      // This will allow us to display that message in an alert:
+      .then((data) => {
+        MySwal.fire({
+          icon: "success",
+          title: data.message
+        });
+        const newRecords = [];
+
+        // Update the table after 2 seconds:
+        setTimeout(() => { setRecords(newRecords) }, 2000);
+      });
+  }
+
+  // This method will restore the default records:
+  function restoreRecords() {
+    const defaultRecords = [{
+      name: "Antonio Celano",
+      position: "Coffee Manager"
+    },
+    {
+      name: "Natalia Jozwiak",
+      position: "Almost CEO, but not quite"
+    },
+    {
+      name: "Manuela Celano",
+      position: "Chairwoman of the Board of Directors"
+    },
+    {
+      name: "Dwight Schrute",
+      position: "Assistant (To The) Regional Manager"
+    },
+    {
+      name: "Perry Cox",
+      position: "J.D.'s (Reluctant) Mentor"
+    },
+    {
+      name: "Rachel Green",
+      position: "We were not on a break"
+    }
+    ];
+
+    fetch("https://mern-list-of-employees-api.vercel.app/home", {
+      method: "POST",
+
+      // The fetch call needs some headers that tell the server how to interpret the request
+      // (it will interpret it as JSON in this case):
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      // The fetch call also needs a body that supplies the data.
+      // The JSON.stringify() method converts a JavaScript value (in this case, the JavaScript object form) to a JSON string:
+      body: JSON.stringify(defaultRecords)
+    })
+      .catch(error => {
+        MySwal.fire({
+          icon: "error",
+          title: error
+        });
+        return;
+      })
+
+      // Since the fetch() function returns a Response object,
+      // we can retrieve the response as JSON using the json() function of the Response object.
+      // The json() method returns a promise which resolves with the result of parsing the body text as JSON.
+      // Note that despite the method being named json(), the result is not JSON
+      // but is instead the result of taking JSON as input and parsing it to produce a JavaScript object.
+      // This allows us to handle the data being sent down to the client-side like a JavaScript object:
+      .then((res) => res.json())
+
+      // Once we have the data returned to us, we get the message property and then put it in a variable called data.
+      // This will allow us to display that message in an alert:
+      .then((data) => {
+        MySwal.fire({
+          icon: "success",
+          title: data.message
+        });
+        // Fetch and update records after restoring defaults:
+        fetchRecords();
+      });
+  }
+
   // This method creates a Record component for each item inside the records collection:
   function listRecord(record) {
     return <Record name={record.name} position={record.position} deleteRecord={deleteRecord} id={record._id} key={record._id} />
@@ -80,6 +194,10 @@ export default function RecordList() {
           }
         </tbody>
       </table>
+      <div className={isMobile ? "mx-auto my-5" : "mx-4 my-5"}>
+        <input type="button" value="Delete All Records" className="btn btn-secondary" onClick={deleteAllRecords} />
+        <input type="button" value="Restore Default Records" className="ms-5 btn btn-secondary" onClick={restoreRecords} />
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -19,8 +19,8 @@ mongoose.connect(atlasUri);
 
 // Mongoose schema
 const recordSchema = new mongoose.Schema({
-    name: String,
-    position: String
+  name: String,
+  position: String
 });
 
 // Mongoose model
@@ -33,76 +33,105 @@ app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 // This section will help you get a list of all the records:
 app.get("/home", (req, res) => {
-    Record.find(function (err, records) {
-        if (err) {
-            res.json(err);
-        } else {
+  Record.find(function (err, records) {
+    if (err) {
+      res.json(err);
+    } else {
 
-            // When the React app makes a GET request to the home route, respond with JSON data (all the records):
-            res.json(records);
-        }
-    });
+      // When the React app makes a GET request to the home route, respond with JSON data (all the records):
+      res.json(records);
+    }
+  });
 });
 
 // This section will help you get a single record by its ID:
 app.get("/edit/:id", function (req, res) {
-    Record.findById(req.params.id, function (err, record) {
-        if (err) {
-            res.json(err);
-        } else {
+  Record.findById(req.params.id, function (err, record) {
+    if (err) {
+      res.json(err);
+    } else {
 
-            // When the React app makes a GET request to the edit route, respond with JSON data (the record with the specific ID):
-            res.json(record);
-        }
-    });
+      // When the React app makes a GET request to the edit route, respond with JSON data (the record with the specific ID):
+      res.json(record);
+    }
+  });
 });
 
 // This section will help you update a record by its ID:
 app.patch("/edit/:id", function (req, res) {
 
-    Record.findByIdAndUpdate(req.params.id, {
-        name: req.body.name,
-        position: req.body.position
-    }, function (err) {
-        if (!err) {
+  Record.findByIdAndUpdate(req.params.id, {
+    name: req.body.name,
+    position: req.body.position
+  }, function (err) {
+    if (!err) {
 
-            // When the React app makes a PATCH request to the /edit/:id route, respond with a JSON object:
-            res.json({ message: "Successfully updated 1 record." });
-        } else {
-            res.json(err);
-        }
-    });
+      // When the React app makes a PATCH request to the /edit/:id route, respond with a JSON object:
+      res.json({ message: "Successfully updated 1 record." });
+    } else {
+      res.json(err);
+    }
+  });
 });
 
 // This section will help you create a new record:
 app.post("/add", function (req, res) {
-    const newRecord = new Record({
-        name: req.body.name,
-        position: req.body.position
-    });
-    newRecord.save(function (err) {
-        if (!err) {
+  const newRecord = new Record({
+    name: req.body.name,
+    position: req.body.position
+  });
+  newRecord.save(function (err) {
+    if (!err) {
 
-            // When the React app makes a POST request to the /add route, respond with a JSON object:
-            res.json({ message: "Successfully added a new record." });
-        } else {
-            res.json(err);
-        }
-    });
+      // When the React app makes a POST request to the /add route, respond with a JSON object:
+      res.json({ message: "Successfully added a new record." });
+    } else {
+      res.json(err);
+    }
+  });
+});
+
+// This section will help you create the default records:
+app.post("/home", function (req, res) {
+  const defaultRecords = req.body;
+  Record.insertMany(defaultRecords, function (err) {
+    if (!err) {
+
+      // When the React app makes a POST request to the /home route, respond with a JSON object:
+      res.json({ message: "Successfully restored default records." });
+    } else {
+      res.json(err);
+    }
+  });
 });
 
 // This section will help you delete a record based on its ID:
 app.delete("/delete/:id", function (req, res) {
 
-    Record.findByIdAndRemove(req.params.id, function (err) {
-        if (!err) {
+  Record.findByIdAndRemove(req.params.id, function (err) {
+    if (!err) {
 
-            // When the React app makes a DELETE request to the /delete/:id route, respond with a JSON object:
-            res.json({ message: "Successfully deleted 1 record." });
-        } else {
-            res.json(err);
-        }
-    });
+      // When the React app makes a DELETE request to the /delete/:id route, respond with a JSON object:
+      res.json({ message: "Successfully deleted 1 record." });
+    } else {
+      res.json(err);
+    }
+  });
+
+});
+
+// This section will help you delete all records:
+app.delete("/home", function (req, res) {
+
+  Record.deleteMany({}, function (err) {
+    if (!err) {
+
+      // When the React app makes a DELETE request to the /home route, respond with a JSON object:
+      res.json({ message: "Successfully deleted all records." });
+    } else {
+      res.json(err);
+    }
+  });
 
 });
 
@@ -110,7 +139,7 @@ app.delete("/delete/:id", function (req, res) {
 
 // All other GET requests not handled before will return our React app:
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-  });
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.listen(port, () => console.log("Server is running on port " + port));
